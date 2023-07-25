f44af3f6bccb4767e66552c382743d80b8ba3441
hyem <devdev.hyem@gmail.com>
2023-07-25 18:52:43 +0900

    feat : 회원가입 페이지 퍼플리싱 작업중

--- src/pages/login/Join.js
+++ src/pages/login/Join.js
@@ -1,143 +1,228 @@
-import React from "react";
-import { Radio, Form, Input } from "antd";
+import React, { useState } from "react";
+import { Radio, Form, Input, ConfigProvider, Modal } from "antd";
 
-import { ButtonConfirm, JoinWrap, RegionSelect, RegionSelectWrap } from "../../style/JoinStyle";
+import {
+  ButtonConfirm,
+  ConfirmArray,
+  JoinWrap,
+  RegionSelectWrap,
+  TermsWarp,
+} from "../../style/JoinStyle";
 import { ButtonOk } from "../../style/GlobalStyle";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import TermsInfo from "../../components/join/TermsInfo";
+import PrivateInfo from "../../components/join/PrivateInfo";
 
 const Join = () => {
+  const [value, setValue] = useState(0);
+  // 약관보기 모달창 관련
+  const checkTerms = () => {
+    Modal.info({
+      title: "WINEY 이용약관",
+      content: (
+        <div>
+          <TermsInfo />
+        </div>
+      ),
+      onOk() {},
+    });
+  };
+  const checkedPrivate = () => {
+    Modal.info({
+      title: "WINEY 개인정보 처리방침",
+      content: (
+        <div>
+          <PrivateInfo />
+        </div>
+      ),
+      onOk() {},
+    });
+  };
+
   const onFinish = values => {
     console.log("Success:", values);
   };
   const onFinishFailed = errorInfo => {
     console.log("Failed:", errorInfo);
   };
-
+  const onChange = e => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
   return (
     <JoinWrap>
-      <Form
-        initialValues={{
-          remember: true,
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#79213d",
+          },
         }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        layout="vertical"
       >
-        <span>아이디(E-mail)</span>
-        <p>사용하실 아이디를 이메일 형식으로 입력해 주세요</p>
-        <Form.Item
-          name="userEmail"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid E-mail!",
-            },
-            {
-              required: true,
-              message: "Please input your E-mail!",
-            },
-            {
-              pattern: /^[\s]/,
-              message: "Please do not use space",
-            },
-          ]}
+        <Form
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          layout="vertical"
         >
-          <Input size="large" />
-          <ButtonConfirm>중복확인</ButtonConfirm>
-        </Form.Item>
+          <span>
+            아이디(E-mail)<b>*</b>
+          </span>
+          <p>사용하실 아이디를 이메일 형식으로 입력해 주세요</p>
+          <ConfirmArray>
+            <Form.Item
+              name="userEmail"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!",
+                },
+                {
+                  pattern: /^[\s]/,
+                  message: "Please do not use space",
+                },
+              ]}
+            >
+              <Input size="large" />
+            </Form.Item>
+            <ButtonConfirm>중복확인</ButtonConfirm>
+          </ConfirmArray>
 
-        <span>비밀번호</span>
-        <p>비밀번호를 입력해 주세요</p>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input.Password size="large" />
-        </Form.Item>
-
-        <span>비밀번호 확인</span>
-        <p>비밀번호를 다시 한번 입력해 주세요</p>
-        <Form.Item
-          name="passwordConfirm"
-          dependencies={["password"]}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(
-                  new Error("The new password that you entered do not match!"),
-                );
+          <span>
+            비밀번호<b>*</b>
+          </span>
+          <p>비밀번호를 입력해 주세요</p>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
               },
-            }),
-          ]}
-        >
-          <Input.Password size="large" />
-        </Form.Item>
-
-        <span>이름</span>
-        <p>이름을 입력해 주세요</p>
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your username!",
-            },
-          ]}
-        >
-          <Input size="large" />
-        </Form.Item>
-
-        <span>연락처</span>
-        <p>연락처를 숫자 형식으로 입력해 주세요</p>
-        <Form.Item
-          name="phoneNumber"
-          rules={[
-            {
-              required: true,
-              message: "Please input your phone number!",
-            },
-          ]}
-        >
-          <Input size="large" /> <ButtonConfirm>본인인증</ButtonConfirm>
-        </Form.Item>
-        <RegionSelectWrap>
-          <Form.Item>
-            <Radio.Group defaultValue="a" size="large">
-              <Radio.Button value="서울">서울</Radio.Button>
-              <Radio.Button value="경기">경기</Radio.Button>
-              <Radio.Button value="인천">인천</Radio.Button>
-              <Radio.Button value="강원">강원</Radio.Button>
-              <Radio.Button value="충남">충남</Radio.Button>
-              <Radio.Button value="대전">대전</Radio.Button>
-              <Radio.Button value="충북">충북</Radio.Button>
-              <Radio.Button value="경북">경북</Radio.Button>
-              <Radio.Button value="대구">대구</Radio.Button>
-              <Radio.Button value="전북">전북</Radio.Button>
-              <Radio.Button value="광주">광주</Radio.Button>
-              <Radio.Button value="전남">전남</Radio.Button>
-              <Radio.Button value="경남">경남</Radio.Button>
-              <Radio.Button value="울산">울산</Radio.Button>
-              <Radio.Button value="부산">부산</Radio.Button>
-              <Radio.Button value="제주">제주</Radio.Button>
-            </Radio.Group>
+            ]}
+          >
+            <Input.Password size="large" />
           </Form.Item>
-        </RegionSelectWrap>
-        <Form.Item>
-          <ButtonOk>회원가입</ButtonOk>
-        </Form.Item>
-      </Form>
+
+          <span>
+            비밀번호 확인<b>*</b>
+          </span>
+          <p>비밀번호를 다시 한번 입력해 주세요</p>
+          <Form.Item
+            name="passwordConfirm"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error(
+                      "The new password that you entered do not match!",
+                    ),
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password size="large" />
+          </Form.Item>
+
+          <span>
+            이름<b>*</b>
+          </span>
+          <p>이름을 입력해 주세요</p>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input size="large" />
+          </Form.Item>
+
+          <span>
+            연락처<b>*</b>
+          </span>
+          <p>연락처를 숫자 형식으로 입력해 주세요</p>
+          <ConfirmArray>
+            <Form.Item
+              name="phoneNumber"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your phone number!",
+                },
+              ]}
+            >
+              <Input size="large" />
+            </Form.Item>
+            <ButtonConfirm>본인인증</ButtonConfirm>
+          </ConfirmArray>
+          <RegionSelectWrap>
+            <span>
+              거주지역<b>*</b>
+            </span>
+            <p>거주지역을 선택해 주세요</p>
+            <Form.Item>
+              <Radio.Group defaultValue="a" size="large">
+                <Radio.Button value="서울">서울</Radio.Button>
+                <Radio.Button value="경기">경기</Radio.Button>
+                <Radio.Button value="인천">인천</Radio.Button>
+                <Radio.Button value="강원">강원</Radio.Button>
+                <Radio.Button value="충남">충남</Radio.Button>
+                <Radio.Button value="대전">대전</Radio.Button>
+                <Radio.Button value="충북">충북</Radio.Button>
+                <Radio.Button value="경북">경북</Radio.Button>
+                <Radio.Button value="대구">대구</Radio.Button>
+                <Radio.Button value="전북">전북</Radio.Button>
+                <Radio.Button value="광주">광주</Radio.Button>
+                <Radio.Button value="전남">전남</Radio.Button>
+                <Radio.Button value="경남">경남</Radio.Button>
+                <Radio.Button value="울산">울산</Radio.Button>
+                <Radio.Button value="부산">부산</Radio.Button>
+                <Radio.Button value="제주">제주</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+          </RegionSelectWrap>
+          <Form.Item>
+            <ButtonOk>회원가입</ButtonOk>
+          </Form.Item>
+        </Form>
+      </ConfigProvider>
+      <TermsWarp>
+        <span>
+          이용약관동의 <b>*</b>
+        </span>
+        <Radio.Group onChange={onChange} value={value}>
+          <Radio value={"all"}>전체 동의합니다.</Radio>
+          <Radio value={1}>이용약관 동의 (필수)</Radio>
+          <span onClick={checkTerms}>
+            약관보기
+            <FontAwesomeIcon icon={faChevronRight} />
+          </span>
+          <Radio value={2}>개인정보 수집·이용 동의 (필수)</Radio>
+          <span onClick={checkedPrivate}>
+            약관보기
+            <FontAwesomeIcon icon={faChevronRight} />
+          </span>
+        </Radio.Group>
+      </TermsWarp>
     </JoinWrap>
   );
 };
--- src/style/JoinStyle.js
+++ src/style/JoinStyle.js
@@ -2,10 +2,31 @@ import styled from "@emotion/styled";
 import { Maincolor, WidthPd } from "./GlobalStyle";
 
 export const JoinWrap = styled.div`
+  margin-top: 3rem;
   padding: ${WidthPd.padding};
   display: flex;
   justify-content: center;
   flex-direction: column;
+  & form span {
+    position: relative;
+    font-size: 1.8rem;
+    font-weight: 700;
+    line-height: 1.7rem;
+    & b {
+      position: absolute;
+      top: -0.2rem;
+      margin-left: 0.2rem;
+      font-size: 1.4rem;
+      color: red;
+    }
+  }
+  & p {
+    font-size: 1.2rem;
+    font-weight: 500;
+    line-height: 1.5rem;
+    margin-left: 0.5rem;
+    padding-bottom: 0.5rem;
+  }
 `;
 export const ButtonConfirm = styled.button`
   display: block;
@@ -14,7 +35,7 @@ export const ButtonConfirm = styled.button`
   border: 0.05rem solid ${Maincolor.grayBold};
   font-size: 1.6rem;
   font-weight: 500;
-  width: 40%;
+  width: 100%;
   height: 4rem;
   border-radius: 0.5rem;
   transition: 0.2s ease-in-out;
@@ -23,21 +44,102 @@ export const ButtonConfirm = styled.button`
   }
 `;
 
-export const RegionSelectWrap = styled.div`
-`
+export const ConfirmArray = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: nowrap;
+  & div {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: nowrap;
 
-export const RegionSelect = styled.button`
-  background: ${Maincolor.redBold};
-  color: ${Maincolor.white};
-  border: 0.05rem solid ${Maincolor.redBold};
-  font-size: 1.4rem;
-  font-weight: 500;
-  width: 6rem;
-  height: 3rem;
-  border-radius: 0.5rem;
-  transition: 0.2s ease-in-out;
-  padding
-  &:hover {
-    background: ${Maincolor.redDeep};
+    & input {
+      margin: 0 auto;
+      width: 41rem;
+    }
+  }
+  & button {
+    margin: 0 auto;
+    width: 15rem;
+    margin-left: 1rem;
+  }
+`;
+export const RegionSelectWrap = styled.div`
+  & div {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    padding: 1px;
+    & label {
+      width: 7rem;
+      padding: 1rem;
+      margin: 0.85rem;
+      border: 0.1rem solid ${Maincolor.grayMedium};
+      border-radius: 0.5rem;
+      &::before {
+        display: none;
+      }
+      & span {
+        position: absolute;
+        top: 1rem;
+        left: 1.8rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: 1.4rem;
+        font-weight: 400;
+      }
+    }
+  }
+`;
+
+export const TermsWarp = styled.div`
+  display: flex;
+  align-content: space-between;
+  flex-direction: column;
+  & span {
+    position: relative;
+    font-size: 1.8rem;
+    font-weight: 700;
+    line-height: 1.7rem;
+    & b {
+      position: absolute;
+      top: -0.2rem;
+      margin-left: 0.2rem;
+      font-size: 1.4rem;
+      color: red;
+    }
+  }
+  & div {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    flex-wrap: wrap;
+    & span {
+      position: relative;
+      font-size: 1.4rem;
+      color: ${Maincolor.redDeep};
+      line-height: 1.7rem;
+      cursor: pointer;
+      & svg {
+        position: absolute;
+        top: 0.5rem;
+        font-weight: 700;
+        font-size: 0.8rem;
+      }
+    }
+    & label {
+      font-size: 1rem;
+      & span {
+        font-size: 1.4rem;
+        color: #000000;
+        font-weight: 500;
+        line-height: 1.7rem;
+      }
+    }
   }
 `;
