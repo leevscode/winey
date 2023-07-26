924def06b246a9462e75b32b7786515f33ff102c
leevscode <leevscode@gmail.com>
2023-07-26 09:30:51 +0900

    feat: ProductCart 작업 중

--- src/pages/product/ProductCart.js
+++ src/pages/product/ProductCart.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { ButtonCancel, ButtonOk } from "../../style/GlobalStyle";
-import { ProductCartNone, ProductCartIn } from "../../style/ProductCartStyle";
+import {
+  ProductCartNone,
+  ProductCartIn,
+  ProductCartInfo,
+} from "../../style/ProductCartStyle";
 import { useNavigate } from "react-router";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
 const ProductCart = () => {
   // 장바구니에 담긴 상품의 수를 추적하는 상태
   const [cartItemCount, setCartItemCount] = useState(0);
@@ -23,17 +28,47 @@ const ProductCart = () => {
   return (
     <>
       {/* 상품이 장바구니에 담겨있지 않을 때 */}
+
       {cartItemCount === 0 ? (
-        <ProductCartNone>장바구니에 담긴 상품이 없습니다.</ProductCartNone>
+        <ProductCartNone>
+          {" "}
+          <div>
+            <i>
+              <FontAwesomeIcon icon={faExclamation} />
+            </i>
+          </div>
+          장바구니에 담긴 상품이 없습니다.
+        </ProductCartNone>
       ) : (
         // 상품이 장바구니에 담겨있을 때
         <ProductCartIn>
-          장바구니에 총 {cartItemCount}개의 상품이 있습니다. 최종결제 금액
-          123456원
+          <div>장바구니에 총 {cartItemCount}개의 상품이 있습니다.</div>
+          <ul>
+            <ProductCartInfo>
+              <div>이미지</div>
+              <span>
+                  <p>제프 까렐, 울띰 헤꼴뜨</p>
+                  <p>Ultime Recolite By Jeff Carrel</p>
+                <span>32,900원</span>
+                <div>
+                  <span>+</span>
+                  <span>2</span>
+                  <span>-</span>
+                </div>
+                </span>
+                <p>x</p>
+              <div>
+             
+                
+               
+                
+              </div>
+            </ProductCartInfo>
+          </ul>
           <ButtonOk>구매하기</ButtonOk>
         </ProductCartIn>
       )}
-      <button onClick={addItemToCart}>상품 추가</button>
+      <button onClick={addItemToCart}>상품 추가 되면 보여요</button>
       <button onClick={removeItemFromCart}>상품 제거</button>
       <ButtonCancel>메인으로 돌아가기</ButtonCancel>
     </>
--- src/style/ProductCartStyle.js
+++ src/style/ProductCartStyle.js
@@ -1,12 +1,17 @@
 import styled from "@emotion/styled";
 
 export const ProductCartNone = styled.div`
-  font-size: 1.4rem;
+  font-size: 2rem;
   padding: 80px;
   text-align: center;
   color: #e7e4e4;
 `;
 
-export const ProductCartIn = styled.div`
-  margin: 100px;
+export const ProductCartIn = styled.div``;
+
+export const ProductCartInfo = styled.div`
+  font-size: 2rem;
+  display: grid;
+  text-align: center;
+  grid-template-columns: 1fr 2.5fr 30px;
 `;
