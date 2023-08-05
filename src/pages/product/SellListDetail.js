daef915b184d3d381a9bead7bffc19c257e4b2b9
leevscode <leevscode@gmail.com>
2023-08-05 12:44:43 +0900

    feat: SellListDetail 더미 데이터 작업 중

--- src/pages/product/SellListDetail.js
+++ src/pages/product/SellListDetail.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {
+  ProductCompleteBox,
+  ProductCompleteinfo,
+} from "../../style/ProductCompleteStyle";
+import { ButtonOk } from "../../style/GlobalStyle";
 
 const SellListDetail = () => {
-  
-  return <div>SellListDetail</div>;
+  return (
+    <div>
+      <ProductCompleteinfo>
+        <img src="https://via.placeholder.com/200x200" alt="" />
+        <ul>
+          <li>제프 까렐, 울띰 헤꼴뜨</li>
+          <li>Ultime Recolte By Jeff Carrel</li>
+          <li>32,900원</li>
+        </ul>
+      </ProductCompleteinfo>
+      <ProductCompleteBox>
+        <ButtonOk>평점 등록</ButtonOk>
+      </ProductCompleteBox>
+    </div>
+  );
 };
 
 export default SellListDetail;
