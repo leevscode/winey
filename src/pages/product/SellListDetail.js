bf460b89d073ddbcef5f7f4f7ceca3f9f94ac3e3
leevscode <leevscode@gmail.com>
2023-08-07 18:50:03 +0900

    feat: SellListDetail 총 금액 작업 중

--- src/pages/product/SellListDetail.js
+++ src/pages/product/SellListDetail.js
@@ -16,16 +16,53 @@ const SellListDetail = () => {
   const [reviewList, setReviewList] = useState([]);
   const [reviewSubmit, setReviewSubmit] = useState(false);
 
+  // 상품 더미 데이터
+  const productData = [
+    {
+      key: 1,
+      imageSrc: "https://via.placeholder.com/120x120",
+      productName: "제프 까렐, 울띰 헤꼴뜨",
+      productDescription: "Ultime Recolte By Jeff Carrel",
+      productPrice: "32,900원",
+    },
+    {
+      key: 2,
+      imageSrc: "https://via.placeholder.com/120x120",
+      productName: "Auth Wine",
+      productDescription: "Auth",
+      productPrice: "52,500원",
+    },
+  ];
+
+  // 결제 총 금액 더미데이터
+  const orderData = {
+    orderDate: "2023.08.07",
+    paymentMethod: "신용카드",
+    pickupLocation: "이마트 만촌점",
+    pickupTime: "07월 23일 일요일 12:00",
+    orderStatus: "픽업완료",
+    totalAmount: 0,
+  };
+
   // 주문 상태가 "픽업대기" 또는 "픽업완료"일 때 선택하고 평점을 등록하는 함수
-  const handlePickUpComplete = (index) => {
+  const handlePickUpComplete = index => {
     setSelectedItem(index);
-    setSelectedOrder((prevSelectedOrder) => {
+    setSelectedOrder(prevSelectedOrder => {
       if (prevSelectedOrder.includes(index)) {
-        return prevSelectedOrder.filter((itemIndex) => itemIndex !== index);
+        return prevSelectedOrder.filter(itemIndex => itemIndex !== index);
       } else {
         return [...prevSelectedOrder, index];
       }
     });
+  };
+
+  // 상품 가격 합산
+  const calculateTotalAmount = () => {
+    let totalPrice = 0;
+    productData.forEach((product) => {
+      totalPrice += product.productPrice;
+    });
+    return totalPrice;
   };
 
   // 리뷰 모달을 여는 함수
@@ -50,15 +87,17 @@ const SellListDetail = () => {
 
   return (
     <>
-      <DetailDay>2023.08.05</DetailDay>
-      <SellListDetailinfo>
-        <img src="https://via.placeholder.com/120x120" alt="" />
-        <ul>
-          <li>제프 까렐, 울띰 헤꼴뜨</li>
-          <li>Ultime Recolte By Jeff Carrel</li>
-          <li>32,900원</li>
-        </ul>
-      </SellListDetailinfo>
+      <DetailDay>{orderData.orderDate}</DetailDay>
+      {productData.map(product => (
+        <SellListDetailinfo key={product.key}>
+          <img src={product.imageSrc} alt="" />
+          <ul>
+            <li>{product.productName}</li>
+            <li>{product.productDescription}</li>
+            <li>{product.productPrice}</li>
+          </ul>
+        </SellListDetailinfo>
+      ))}
       <SellListDetailBox>
         {/* 평점이 등록되지 않았을 때만 버튼을 보여줌 */}
         {!reviewSubmit && <ButtonOk onClick={showModal}>평점등록</ButtonOk>}
@@ -66,12 +105,13 @@ const SellListDetail = () => {
         {reviewSubmit && renderReviewMessage()}
       </SellListDetailBox>
       <DetailTotalPrice>
-        <p>결제 방법 : 신용카드</p>
-        <p>픽업 지점 : 이마트 만촌점</p>
-        <p>픽업 시간 : 07월 23일 일요일 12:00</p>
-        <p>주문 상태 : 픽업완료</p>
+        <p>결제 방법: {orderData.paymentMethod}</p>
+        <p>픽업 지점: {orderData.pickupLocation}</p>
+        <p>픽업 시간: {orderData.pickupTime}</p>
+        <p>주문 상태: {orderData.orderStatus}</p>
         <p>
-          총 결제금액 <strong>원</strong>
+        총 결제금액{" "}
+          <strong>{calculateTotalAmount().toLocaleString()}원</strong>
         </p>
       </DetailTotalPrice>
 
