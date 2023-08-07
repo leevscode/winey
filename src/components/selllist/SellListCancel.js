b6881a6d145129a2b7241ff69bd7ed478530c26b
leevscode <leevscode@gmail.com>
2023-08-07 17:37:24 +0900

    feat: ReviewModal 평점등록 기능 추가

--- src/components/selllist/SellListCancel.js
+++ src/components/selllist/SellListCancel.js
@@ -6,15 +6,16 @@ import {
 } from "../../style/SellListCancelStyle";
 import { ButtonOk, ButtonCancel } from "../../style/GlobalStyle";
 
-const SellListCancel = ({ onCancel, onClose  }) => {
+const SellListCancel = ({ onCancel, onClose }) => {
   return (
     <>
       <OrderCancelModal>
         <OrderCancelContent>
           <OrderCancelText>정말로 취소 하시겠습니까?</OrderCancelText>
           <ButtonOk onClick={onCancel}>네</ButtonOk>
-          <br />
-          <ButtonCancel onClick={onClose}>아니요</ButtonCancel>
+          <div style={{ marginTop: "8px" }}>
+            <ButtonCancel onClick={onClose}>아니요</ButtonCancel>
+          </div>
         </OrderCancelContent>
       </OrderCancelModal>
     </>
--- src/pages/product/SellListDetail.js
+++ src/pages/product/SellListDetail.js
@@ -11,10 +11,11 @@ import ReviewModal from "../../components/selllist/ReviewModal";
 
 const SellListDetail = () => {
   const [reviewReset, setreviewReset] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null); 
-  const [selectedOrder, setSelectedOrder] = useState([]); 
-  const [reviewList, setReviewList] = useState([]); 
-  
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedOrder, setSelectedOrder] = useState([]);
+  const [reviewList, setReviewList] = useState([]);
+  const [reviewSubmit, setReviewSubmit] = useState(false);
+
   // 주문 상태가 "픽업대기" 또는 "픽업완료"일 때 선택하고 평점을 등록하는 함수
   const handlePickUpComplete = (index) => {
     setSelectedItem(index);
@@ -38,25 +39,13 @@ const SellListDetail = () => {
   };
 
   // 평점 등록이 완료되었을 때 보여줄 메시지 div
-  const [reviewSubmit, setreviewSubmit] = useState(false);
-
-  // 평점 등록이 완료된 항목만 상태를 업데이트 하여야 한다.
-  const reviewSubmitUpdate = () => {
-    console.log(selectedItem, "만 업데이트 하여야 한다. ");
-    const arr = reviewList.map((item, index) => {
-      if (index === selectedItem) {
-        return true;
-      } else {
-        return item;
-      }
-    });
-    setReviewList([...arr]);
-    setreviewSubmit(true);
+  const renderReviewMessage = () => {
+    return <ReviewOk>평점등록이 완료되었습니다</ReviewOk>;
   };
 
-  const tempTest = () => {
-    setreviewSubmit(false);
-    return <ReviewOk>평점등록이 완료되었습니다</ReviewOk>;
+  // 평점 등록이 완료된 항목만 상태를 업데이트
+  const reviewSubmitUpdate = () => {
+    setReviewSubmit(true);
   };
 
   return (
@@ -71,7 +60,10 @@ const SellListDetail = () => {
         </ul>
       </SellListDetailinfo>
       <SellListDetailBox>
-        <ButtonOk onClick={showModal}>평점등록</ButtonOk>
+        {/* 평점이 등록되지 않았을 때만 버튼을 보여줌 */}
+        {!reviewSubmit && <ButtonOk onClick={showModal}>평점등록</ButtonOk>}
+        {/* 리뷰 제출 완료 시 메시지를 보여줌 */}
+        {reviewSubmit && renderReviewMessage()}
       </SellListDetailBox>
       <DetailTotalPrice>
         <p>결제 방법 : 신용카드</p>
@@ -79,7 +71,7 @@ const SellListDetail = () => {
         <p>픽업 시간 : 07월 23일 일요일 12:00</p>
         <p>주문 상태 : 픽업완료</p>
         <p>
-          총 결제금액 <strong> 원</strong>
+          총 결제금액 <strong>원</strong>
         </p>
       </DetailTotalPrice>
 
@@ -87,7 +79,7 @@ const SellListDetail = () => {
       <ReviewModal
         reviewReset={reviewReset}
         hideModal={hideModal}
-        setreviewSubmit={setreviewSubmit}
+        setReviewSubmit={setReviewSubmit}
         reviewSubmitUpdate={reviewSubmitUpdate}
       />
     </>
