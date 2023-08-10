9f183708ace516c2e5deb5cb863e6e0ac22b4c98
leevscode <leevscode@gmail.com>
2023-08-10 19:41:03 +0900

    feat : product 수정 작업

--- src/api/patchselllist.js
+++ src/api/patchselllist.js
@@ -1,5 +1,16 @@
-import axios from "axios";
 import { client } from "./client";
+
+// 주문 내역 출력
+export const fetchSellListData = async () => {
+  try {
+    const response = await client.get(`orderList/user`);
+    const sellListData = response.data;
+    return sellListData;
+  } catch (error) {
+    console.error("API 요청 중 오류 발생", error);
+    return [];
+  }
+};
 
 // 리뷰를 등록하는 함수
 const submitReview = reviewData => {
