dbef57adcd6f572ed60e3cd9c65a2f22a66d917f
kimaydev <kimaydev@gmail.com>
2023-08-14 15:28:57 +0900

    docs: 노션 주소 수정

--- src/components/main/RecommWine.js
+++ src/components/main/RecommWine.js
@@ -1,6 +1,6 @@
 /*
   작업자 : 김아영
-  노션 : https://www.notion.so/kimaydev
+  노션 : https://kimaydev.notion.site/kimaydev/FE-7a53f9f631f146c88c39413cd175a9d0
   깃허브 : https://github.com/kimaydev
 */
 import React, { useEffect, useState } from "react";
@@ -17,7 +17,7 @@ const RecommWine = ({ setIsModalOpen }) => {
 
   const userData = useSelector(state => state.user);
 
-  useEffect(() => {    
+  useEffect(() => {
     if (userData.userId === null) {
       console.log("로그아웃상태");
     } else {
