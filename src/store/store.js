c663993204b130d8e478bdd96b7a5a12933c5705
hyemdev <devdev.hyem@gmail.com>
2023-08-22 14:27:23 +0900

    feat : redux persist 적용

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../reducers/cartSlice";
 import userSlice from "../reducers/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
+// persist config 작성
+const persistConfig = {
+  key: "root", // localStorage key
+  storage, // localStorage
+  whitelist: ["user", "cart"], // target (reducer name)
+};
+const reducer = combineReducers({
+  cart: cartReducer,
+  user: userSlice,
+});
+const persistedReducer = persistReducer(persistConfig, reducer);
 const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userSlice,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
