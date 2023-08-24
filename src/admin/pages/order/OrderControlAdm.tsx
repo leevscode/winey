a08e510885e526480bf0b9d0eaa31988a1dc5309
leevscode <leevscode@gmail.com>
2023-08-24 21:04:24 +0900

    feat: AdminOrderControl 레이아웃 수정 중

--- src/admin/pages/order/OrderControlAdm.tsx
+++ src/admin/pages/order/OrderControlAdm.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchOrderControlData } from "../../api/admorderlist";
+import { OrderTableWrap, OrdertTable } from "../../style/AdminOrderControl";
 
 const OrderControlAdm = () => {
   const [orderControl, setOrderControl] = useState([]);
@@ -17,11 +18,90 @@ const OrderControlAdm = () => {
     fetchData();
   }, []);
 
+  const tableData = [
+    {
+      idata: 1,
+      data_a: "248",
+      data_b: "auth@naver.com",
+      data_c: "1862 까베르네",
+      data_d: "1",
+      data_e: "36,800원",
+      data_f: "신용카드",
+      data_g: "대구상인점",
+      data_h: "배송중",
+      data_i: "상세보기",
+    },
+    {
+      idata: 2,
+      data_a: "248",
+      data_b: "auth@naver.com",
+      data_c: "1862 까베르네",
+      data_d: "1",
+      data_e: "36,800원",
+      data_f: "신용카드",
+      data_g: "대구상인점",
+      data_h: "배송중",
+      data_i: "상세보기",
+    },
+    {
+      idata: 3,
+      data_a: "248",
+      data_b: "auth@naver.com",
+      data_c: "1862 까베르네",
+      data_d: "1",
+      data_e: "36,800원",
+      data_f: "신용카드",
+      data_g: "대구상인점",
+      data_h: "배송중",
+      data_i: "상세보기",
+    },
+  ];
+
   return (
     <div>
-      주문내역관리
-      <ul>
-        {/* {orderControl.map((item, key) => (
+      <OrderTableWrap>
+        <OrdertTable>
+          <caption>주문내역관리</caption>
+          <thead>
+            <tr>
+              <th>주문날짜</th>
+              <th>아이디</th>
+              <th>주문상품</th>
+              <th>주문수량</th>
+              <th>결제금액</th>
+              <th>결제수단</th>
+              <th>픽업장소</th>
+              <th>픽업배송상태</th>
+              <th>상세보기</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map(item => (
+              <tr key={item.idata}>
+                <td>{item.data_a}</td>
+                <td>{item.data_b}</td>
+                <td>{item.data_c}</td>
+                <td>{item.data_d}</td>
+                <td>
+                  <p>{item.data_e}</p>
+                </td>
+                <td>{item.data_f}</td>
+                <td>{item.data_g}</td>
+                <td>{item.data_h}</td>
+                <td>{item.data_i}</td>
+              </tr>
+            ))}
+          </tbody>
+        </OrdertTable>
+      </OrderTableWrap>
+    </div>
+  );
+};
+
+{
+  /* 주문내역관리
+      {/* <ul>
+        {orderControl.map((item, key) => (
           <li key={key}>
             주문번호: {item.orderId}
             주문날짜: {item.orderDate}
@@ -34,10 +114,7 @@ const OrderControlAdm = () => {
             배송상태: {item.count}
             상세보기
           </li>
-        ))} */}
-      </ul>
-    </div>
-  );
-};
-
+        ))}
+      </ul> */
+}
 export default OrderControlAdm;
---
+++ src/admin/style/AdminOrderControl.ts
@@ -0,0 +1,66 @@
+import styled from "@emotion/styled";
+import {
+  Gradation,
+  Maincolor,
+  WidthPd,
+  opacity,
+} from "../../style/GlobalStyle";
+import { AdminColor } from "./AdminLayoutStyle";
+
+// 테이블 감싸는 div
+export const OrderTableWrap = styled.div`
+  /* background: pink; */
+  overflow: auto;
+  padding-bottom: 10px;
+  /* 스크롤바 커스텀 */
+  &::-webkit-scrollbar {
+    display: block;
+    width: 6px;
+    height: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${opacity.gray};
+    border-radius: 10px;
+  }
+`;
+
+export const OrdertTable = styled.table`
+  width: 100%;
+  min-width: 1000px;
+  background: ${Maincolor.white};
+  border: 0.05rem solid ${opacity.white};
+  th,
+  td {
+    padding: 15px 5px;
+    border-right: 0.05rem solid ${opacity.white};
+    vertical-align: middle;
+    &:last-of-type {
+      border-right: 0;
+    }
+  }
+  thead {
+    tr {
+      background: ${AdminColor.headColorA};
+      th {
+        border-bottom: 0.2rem solid ${opacity.white};
+        font-weight: 700;
+        color: ${Maincolor.white};
+        min-width: 100px;
+      }
+    }
+  }
+  tbody {
+    tr {
+      transition: 0.1s ease-in-out background;
+      &:hover {
+        background: ${AdminColor.bodyColorA};
+      }
+      td {
+        border-bottom: 0.05rem solid ${opacity.white};
+      }
+    }
+  }
+`;
