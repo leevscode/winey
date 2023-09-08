fc4aaf9539cd877510af0b0b6280f17e0689d219
kimaydev <kimaydev@gmail.com>
2023-09-08 18:09:48 +0900

    refactor: 관리자 페이지 매장 등록 화면 스타일 수정 작업 진행중

--- src/admin/pages/salestore/StoreAddAdm.tsx
+++ src/admin/pages/salestore/StoreAddAdm.tsx
@@ -3,14 +3,21 @@
     노션 : https://hyemdev.notion.site/hyemdev/hyem-s-dev-STUDY-75ffe819c7534a049b59871e6fe17dd4
     깃허브 : https://github.com/hyemdev
 */
-import { Form, Input, Modal, Radio, RadioChangeEvent } from "antd";
+import {
+  ConfigProvider,
+  Form,
+  Input,
+  Modal,
+  Radio,
+  RadioChangeEvent,
+} from "antd";
 import React, { useEffect, useState } from "react";
 import { regionOptions } from "../member/MemberControlAdm";
 import { StoreAddWrap, StoreAddressModal } from "../../style/AdminStoreStyle";
 import { postNewStore } from "../../api/patchAdmStore";
 import { IStoreDetailList } from "../../interface/StoreInterface";
 import DaumPostcode from "react-daum-postcode";
-import { DetailBt } from "../../style/AdminLayoutStyle";
+import { AdminColor, DetailBt } from "../../style/AdminLayoutStyle";
 import { useNavigate } from "react-router";
 import {
   AdmProductBtnCancel,
@@ -166,112 +173,119 @@ const StoreAddAdm: React.FC = () => {
 
   return (
     <StoreAddWrap>
-      <Form
-        onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
-        layout="horizontal"
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: AdminColor.yellowB,
+          },
+        }}
       >
-        <div className="addButton">
-          <AdmProductBtnOk
-          // onClick={handleStoreSave}
-          >
-            저장하기
-          </AdmProductBtnOk>
-          <AdmProductBtnCancel onClick={onCancel}>취소하기</AdmProductBtnCancel>
-        </div>
-
-        <div className="storeAddForm">
-          <ul>
-            <li>지역선택</li>
-            <li>
-              <Form.Item
-                name="regionSelect"
-                rules={[
-                  {
-                    required: true,
-                    message: "지역을 선택해 주세요",
-                  },
-                ]}
-              >
-                <Radio.Group onChange={e => handleCity(e)} size="large">
-                  {regionOptions.map(item => (
-                    <Radio key={item.regionNmId} value={item.regionNmId}>
-                      {item.value}
-                    </Radio>
-                  ))}
-                </Radio.Group>
-              </Form.Item>
-            </li>
-          </ul>
-          <ul>
-            <li>매장 이름</li>
-            <li>
-              <Form.Item
-                name="storeNm"
-                rules={[
-                  {
-                    required: true,
-                    message: "매장 이름을 입력해 주세요",
-                  },
-                ]}
-              >
-                <Input
-                  placeholder="매장 이름 입력하세요."
-                  onChange={data => handleStoreName(data)}
-                />
-              </Form.Item>
-            </li>
-          </ul>
-          <ul>
-            <li>매장 주소</li>
-            <li>
-              <Form.Item className="storeAddressSt" name="storeAddress">
-                <Input
-                  value={calendarlocation}
-                  placeholder="매장 주소를 입력하세요."
-                  // readOnly
-                  onClick={handleAddress.clickButton}
-                />
-                <Input
-                  className="storeAddressSub"
-                  value={subAddress}
-                  placeholder="상세주소"
-                  onChange={handleAddressSub}
-                />
-                <DetailBt onClick={handleAddress.clickButton}>
-                  주소검색
-                </DetailBt>
-              </Form.Item>
-              {addressErr ? <p>{addressErr}</p> : <p></p>}
-            </li>
-          </ul>
-          <ul>
-            <li>연락처</li>{" "}
-            <li>
-              <Form.Item
-                name="storeNumber"
-                rules={[
-                  {
-                    pattern: /^(\d{2,3})?-?(\d{3,4})?-?(\d{4})$/,
-                    message: "형식에 맞춰 입력해 주세요. (ex. 000-000-0000)",
-                  },
-                  {
-                    required: true,
-                    message: "매장 연락처를 입력해 주세요",
-                  },
-                ]}
-              >
-                <Input
-                  // 글자수 제한
-                  maxLength={13}
-                  placeholder="매장 연락처를 형식에 맞춰 입력해 주세요(ex. 000-000-0000)"
-                  onChange={e => handleStoreNumber(e)}
-                />
-              </Form.Item>
-            </li>
-          </ul>
-        </div>
-      </Form>
+        <Form
+          onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          layout="horizontal"
+        >
+          <div className="addButton">
+            <AdmProductBtnOk
+            // onClick={handleStoreSave}
+            >
+              매장 등록
+            </AdmProductBtnOk>
+            <AdmProductBtnCancel onClick={onCancel}>취소</AdmProductBtnCancel>
+          </div>
+          <div className="storeAddForm">
+            <ul>
+              <li className="title">지역선택</li>
+              <li className="content">
+                <Form.Item
+                  name="regionSelect"
+                  rules={[
+                    {
+                      required: true,
+                      message: "지역을 선택해 주세요",
+                    },
+                  ]}
+                >
+                  <Radio.Group onChange={e => handleCity(e)} size="large">
+                    {regionOptions.map(item => (
+                      <Radio key={item.regionNmId} value={item.regionNmId}>
+                        {item.value}
+                      </Radio>
+                    ))}
+                  </Radio.Group>
+                </Form.Item>
+              </li>
+            </ul>
+            <ul>
+              <li className="title">매장 이름</li>
+              <li className="content">
+                <Form.Item
+                  name="storeNm"
+                  rules={[
+                    {
+                      required: true,
+                      message: "매장 이름을 입력해 주세요",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="매장 이름 입력하세요."
+                    onChange={data => handleStoreName(data)}
+                  />
+                </Form.Item>
+              </li>
+            </ul>
+            <ul>
+              <li className="title">매장 주소</li>
+              <li className="content">
+                <Form.Item className="storeAddressSt" name="storeAddress">
+                  <Input
+                    value={calendarlocation}
+                    placeholder="매장 주소를 입력하세요."
+                    // readOnly
+                    onClick={handleAddress.clickButton}
+                  />
+                  <Input
+                    className="storeAddressSub"
+                    value={subAddress}
+                    placeholder="상세주소"
+                    onChange={handleAddressSub}
+                  />
+                  <DetailBt onClick={handleAddress.clickButton}>
+                    주소검색
+                  </DetailBt>
+                </Form.Item>
+                {addressErr ? <p>{addressErr}</p> : <p></p>}
+              </li>
+            </ul>
+            <ul>
+              <li className="title">연락처</li>
+              <li className="content">
+                <Form.Item
+                  name="storeNumber"
+                  rules={[
+                    {
+                      pattern: /^(\d{2,3})?-?(\d{3,4})?-?(\d{4})$/,
+                      message: "형식에 맞춰 입력해 주세요. (ex. 000-000-0000)",
+                    },
+                    {
+                      required: true,
+                      message: "매장 연락처를 입력해 주세요",
+                    },
+                  ]}
+                >
+                  <Input
+                    // 글자수 제한
+                    maxLength={13}
+                    placeholder="매장 연락처를 형식에 맞춰 입력해 주세요(ex. 000-000-0000)"
+                    onChange={e => handleStoreNumber(e)}
+                  />
+                </Form.Item>
+              </li>
+            </ul>
+          </div>
+        </Form>
+      </ConfigProvider>
     </StoreAddWrap>
   );
 };
--- src/admin/style/AdminStoreStyle.ts
+++ src/admin/style/AdminStoreStyle.ts
@@ -4,50 +4,67 @@
     깃허브 : https://github.com/hyemdev
 */
 import styled from "@emotion/styled";
-import { Maincolor, WidthPd } from "../../style/GlobalStyle";
+import { Maincolor, WidthPd, opacity } from "../../style/GlobalStyle";
 import { AdminColor } from "./AdminLayoutStyle";
 import exp from "constants";
 
 export const StoreAddWrap = styled.div`
-  padding: ${WidthPd.padding};
+  /* padding: ${WidthPd.padding}; */
   margin: 0 auto;
   .addButton {
-    text-align: right;
-    margin-right: 4%;
+    position: sticky;
+    top: 0;
+    z-index: 100;
+    display: flex;
+    justify-content: flex-end;
+    gap: 0 5px;
+    background: ${Maincolor.white};
+    border-bottom: 0.05rem solid ${opacity.white};
+    padding-bottom: 10px;
     & > button {
-      margin: 0 1rem;
+      /* margin: 0 1rem; */
     }
   }
   .storeAddForm {
-    margin-top: 5rem;
-    & ul:first-of-type {
-      & > li:last-of-type {
-        padding: 2rem;
-      }
-    }
-    & ul {
+    /* margin-top: 5rem; */    
+    & > ul {
       display: flex;
       justify-content: center;
+      gap: 0 10px;
+      border-bottom: 0.05rem solid ${opacity.white};
       /* align-items: center; */
-      & li:first-of-type {
+      & > li{
+        padding: 10px 0;
+      }
+      .title {
+        width: 130px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         background: ${AdminColor.headColorD};
-        width: 20%;
-        height: 8rem;
-        line-height: 8rem;
+        font-weight: 700;
+      }
+      .content {
+        width: calc(100% - 130px - 10px);
+      }  
+      /* & li:first-of-type {
+        /* width: 20%; */
+        /* height: 8rem; */
+        /* line-height: 8rem; */
         text-align: center;
       }
       & li:last-of-type {
         border: 1px soild ${AdminColor.headColorD};
-        padding: 2.5rem 2rem;
-        width: 75%;
-        height: 8rem;
+        /* padding: 2.5rem 2rem; */
+        /* width: 75%; */
+        /* height: 8rem; */
         & > p {
           color: #ff4d4f;
         }
         & > div {
           margin: 0;
         }
-      }
+      } */
       .errorString {
         font-size: 1.4rem;
         color: red;
