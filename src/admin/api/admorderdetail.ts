92b12e7925eec5508aac951164a7d50aea62e7c8
leevscode <leevscode@gmail.com>
2023-09-13 09:52:53 +0900

    깃허브 push 테스트

--- src/admin/api/admorderdetail.ts
+++ src/admin/api/admorderdetail.ts
@@ -12,7 +12,6 @@ export const AdmOrderDetailData = async (orderId: number) => {
       `/api/admin/order/${orderId}?page=0&size=20&sort=orderid%2CASC`,
     );
     const orderDetailData = await res.data;
-    // console.log(orderDetailData);
     return orderDetailData;
   } catch (error) {
     console.error("API 요청 중 오류 발생", error);
